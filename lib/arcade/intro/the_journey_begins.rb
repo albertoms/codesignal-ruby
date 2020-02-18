3c5b70ec22ed7120234b5f9a6d69ffd48886de29
albertoms <albertoguerz@gmail.com>
2020-02-18 08:13:53 -0600

    check_palindrome done

--- lib/arcade/intro/the_journey_begins.rb
+++ lib/arcade/intro/the_journey_begins.rb
@@ -1,11 +1,24 @@
 class TheJourneyBegins
-    def self.add(param1, param2)
+    def add(param1, param2)
         param1 + param2
     end
 
-    def self.century_from_year(year)
+    def century_from_year(year)
         # You have to add a decimal to convert a number to float
         century = year / 100.0
         century.ceil
+    end
+
+    def check_palindrome(inputString)
+        # inputString == inputString.reverse
+        strLen = inputString.length
+
+        for i in 0..strLen/2 do
+            if !(inputString[i] == inputString[(strLen - 1) - i]) then
+                return false
+            end
+        end
+
+        true
     end
 end
--- test/arcade/intro/test_the_journey_begins.rb
+++ test/arcade/intro/test_the_journey_begins.rb
@@ -2,15 +2,25 @@ require 'minitest/autorun'
 require_relative '../../../lib/arcade/intro/the_journey_begins'
 
 class TheJourneyBeginsTest < Minitest::Test
+    def setup
+        @this_world = TheJourneyBegins.new
+    end
+
     def test_add
-        assert_equal 3, TheJourneyBegins.add(1, 2)
-        assert_equal (-37), TheJourneyBegins.add(2, -39)
-        assert_equal (-2000), TheJourneyBegins.add(-1000, -1000)
+        assert_equal 3, @this_world.add(1, 2)
+        assert_equal (-37), @this_world.add(2, -39)
+        assert_equal (-2000), @this_world.add(-1000, -1000)
     end
 
     def test_century_from_year
-        assert_equal 20, TheJourneyBegins.century_from_year(1905)
-        assert_equal 17, TheJourneyBegins.century_from_year(1700)
-        assert_equal 1, TheJourneyBegins.century_from_year(8)
+        assert_equal 20, @this_world.century_from_year(1905)
+        assert_equal 17, @this_world.century_from_year(1700)
+        assert_equal 1, @this_world.century_from_year(8)
+    end
+
+    def test_check_palindrome
+        assert_equal true, @this_world.check_palindrome("aabaa")
+        assert_equal false, @this_world.check_palindrome("zzzazzazz")
+        assert_equal true, @this_world.check_palindrome("z")
     end
 end
