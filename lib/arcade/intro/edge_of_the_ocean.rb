bbc235b647eed95440db74b2cd2725f395d05085
albertoms <albertoguerz@gmail.com>
2020-02-28 09:22:10 -0600

    shape_area, make_array_consecutive2, almost_increasing_sequence done

--- lib/arcade/intro/edge_of_the_ocean.rb
+++ lib/arcade/intro/edge_of_the_ocean.rb
@@ -1,6 +1,6 @@
 class EdgeOfTheOcean
     def adjacent_elements_product(inputArray)
-        largest = -1000 * 1000;
+        largest = -1000 * 1000
 
         for i in 0..(inputArray.size - 2) do
             this_product = inputArray[i] * inputArray[i + 1]
@@ -12,4 +12,47 @@ class EdgeOfTheOcean
     
         return largest
     end
+
+    def shape_area(n)
+        return n ** 2 + (n-1) ** 2
+    end
+
+    def make_array_consecutive2(statues)
+        statues.max - statues.min - (statues.size - 1)
+    end
+
+    # Rule:
+    # To obtain a strictly increasing sequence by removing
+    # no more than one element from the array.
+    def almost_increasing_sequence(sequence)
+        ints_to_remove = 0
+
+        for i in 0..(sequence.size - 2) do
+            cur_int = sequence[i]
+            next_int = sequence[i + 1]
+            
+            # current integer should be removed to meet the rule
+            if cur_int >= next_int
+                ints_to_remove += 1
+            end
+            
+            # make sure that by removing the current integer
+            # previous and 2nd next items would meet the rule
+            # When sequence = [40, 50, 60, 10, 20, 30] and cur_int = 60:
+            if  i > 0 &&
+                (i + 2) < sequence.size &&
+                sequence[i - 1] >= next_int && # 50 >= 10
+                cur_int >= sequence[i + 2] # 60 >= 20
+            then
+                ints_to_remove += 1
+            end
+
+            if ints_to_remove > 1
+                break
+            end
+        end
+        
+        return ints_to_remove < 2
+    end
+
 end
--- test/arcade/intro/test_edge_of_the_ocean.rb
+++ test/arcade/intro/test_edge_of_the_ocean.rb
@@ -11,4 +11,23 @@ class TestEdgeOfTheOcean < Minitest::Test
         assert_equal (-12), @this_world.adjacent_elements_product([-23, 4, -3, 8, -12])
         assert_equal 0, @this_world.adjacent_elements_product([1, 0, 1, 0, 1000])
     end
+
+    def test_shape_area
+        assert_equal 5, @this_world.shape_area(2)
+        assert_equal 1, @this_world.shape_area(1)
+        assert_equal 199900013, @this_world.shape_area(9998)
+    end
+
+    def test_make_array_consecutive2
+        assert_equal 3, @this_world.make_array_consecutive2([6, 2, 3, 8])
+        assert_equal 0, @this_world.make_array_consecutive2([1])
+        assert_equal 2, @this_world.make_array_consecutive2([0, 3])
+    end
+
+    def test_almost_increasing_sequence
+        assert_equal false, @this_world.almost_increasing_sequence([1, 3, 2, 1])
+        assert_equal true, @this_world.almost_increasing_sequence([1, 2, 5, 3, 5])
+        assert_equal true, @this_world.almost_increasing_sequence([3, 5, 67, 98, 3])
+    end
+
 end
